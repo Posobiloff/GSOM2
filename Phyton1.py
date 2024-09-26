51758c037740ad4842cebbbf8abf2f21364b470f
Fleymus <134969269+Fleymus@users.noreply.github.com>
2024-09-26 13:32:34 +0300

    Update Phyton1.py
    Improvement of the code, now it won't break when given value error

--- Phyton1.py
+++ Phyton1.py
@@ -1,16 +1,25 @@
 # 1. INT, FLOAT. The simple code that computes the discriminant using only int numbers.
-print("Now we're going to calculate discriminant for a*x2+b*x+c=0")
-a = int(input("Input variable a"))
-b = int(input("Input variable b"))
-c = int(input("Input variable c"))
-D = b * b - 4*a*c
+print("Now we're going to calculate discriminant for a*x² + b*x + c = 0")
+
+def get_numeric_input(number):
+    while True:
+        try:
+            return float(input(number))
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+
+a = get_numeric_input("Input variable a: ")
+b = get_numeric_input("Input variable b: ")
+c = get_numeric_input("Input variable c: ")
+
+D = b * b - 4 * a * c
+
 if D < 0:
-    print ("No roots, D<0")
+    print("No roots, D < 0")
 elif D == 0:
-    x = -b/(2*a)
-    print (x)
+    x = -b / (2 * a)
+    print(f"One root: {x}")
 else:
-    x1 = float((-b-D**0.5)/(2*a))
-    x2 = float((-b+D**0.5)/(2*a))
-    print (x1)
-    print (x2)
+    x1 = (-b - D**0.5) / (2 * a)
+    x2 = (-b + D**0.5) / (2 * a)
+    print(f"Two roots: {x1} and {x2}")
